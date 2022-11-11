a8cdaf7580f5f00c45a050d05ed4990c86c8787a
Daenielle May Felipe <daeniellemay.felipe@edu.sait.ca>
2022-11-10 18:54:53 -0700

    fixed website layout and updated skills and projects

--- src/components/Skills/skills-data.js
+++ src/components/Skills/skills-data.js
@@ -2,7 +2,6 @@ import CSS_ICON from "../../assets/skills/css3.svg";
 import HTML_ICON from "../../assets/skills/html-5.svg";
 import NODE_ICON from "../../assets/skills/nodejs.svg";
 import JAVASCRIPT_ICON from "../../assets/skills/javascript.svg";
-import MYSQL_ICON from "../../assets/skills/mysql.svg";
 import REACT_ICON from "../../assets/skills/react.svg";
 import STYLED_ICON from "../../assets/skills/styled-components.svg";
 import GIT_ICON from "../../assets/skills/git-icon.svg";
